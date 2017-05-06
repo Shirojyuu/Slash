9df2729621b310f1a14d2b1e2f6c47699d0bd1db
Alec Day <shirojyuu@gmail.com>
2017-05-05 22:49:44 -0400

    Basic running and jumping works fine.

---
+++ Assets/_Scripts/Game/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start()
+    {
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    void LateUpdate () {
+		if (player != null) {
+			transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z - 1);
+		}
+	}    
+
+}
--- Assets/_Scripts/Player/ScoutController.cs
+++ Assets/_Scripts/Player/ScoutController.cs
@@ -7,9 +7,14 @@ using UnityEngine;
 public class ScoutController : MonoBehaviour {
     private Rigidbody2D rb;
     private Animator anim;
-    private float runSpeed;
 
-	void Start () {
+    private bool grounded;
+
+    public float runSpeed;
+    public float jumpStrength;
+    public float checkDist = 3.0f;
+
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 	}
@@ -18,11 +23,49 @@ public class ScoutController : MonoBehaviour {
 		
 	}
 
+    private void FixedUpdate()
+    {
+        //Ground Check  
+        Vector2 checkPos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.size.y / 2.0f);
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(checkPos, checkDist * Vector2.down, checkDist);
+        Debug.DrawRay(checkPos, Vector2.down * checkDist, Color.green);
+
+        if (groundCheck.collider != null)
+        {
+            Debug.Log(groundCheck.collider);
+            grounded = true;
+        }
+
+        else
+        {
+            grounded = false;
+        }
+
+        anim.SetBool("Grounded", grounded);
+
+        Debug.Log(grounded);
+        //All you'll ever be doing is running right...so...
+        Vector2 movement = new Vector2(runSpeed, 0.0f);
+        rb.AddForce(movement);
+
+        //Any time actions:
+        HandleJump();
+        HandleBrake();
+        HandleSlash();
+    }
+
 
     //All of Scout's Moveset
     private void HandleJump()
     {
-       
+        if (Input.GetButtonDown("Action") && grounded)
+        {
+            Input.ResetInputAxes();
+            Vector2 jump = new Vector2(rb.velocity.x, jumpStrength);
+            anim.SetTrigger("Jump");
+            rb.AddForce(jump);
+        }
     }
 
     private void HandleBrake()
@@ -34,8 +77,17 @@ public class ScoutController : MonoBehaviour {
     /*
      * 0 = Front, 1 = Up, 2 = Down, 3 = Air
      * */
-    private void HandleSlash(int direction)
+    private void HandleSlash()
     {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Equals("Loop"))
+        {
+            Transform loopPoint = GameObject.FindGameObjectWithTag("Respawn").transform;
+            transform.position = loopPoint.position;
+        }
     }
 }
