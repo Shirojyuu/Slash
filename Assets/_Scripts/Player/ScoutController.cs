f6534a1fe84ff0bda4b09aaabfdbbd6dff5e3392
Alec Day <shirojyuu@gmail.com>
2017-05-05 21:28:39 -0400

    Initial Commit

---
+++ Assets/_Scripts/Player/ScoutController.cs
@@ -0,0 +1,41 @@
+﻿/*2017 Alec Day*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoutController : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float runSpeed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		
+	}
+
+
+    //All of Scout's Moveset
+    private void HandleJump()
+    {
+       
+    }
+
+    private void HandleBrake()
+    {
+
+    }
+
+    //Slash Encodings:
+    /*
+     * 0 = Front, 1 = Up, 2 = Down, 3 = Air
+     * */
+    private void HandleSlash(int direction)
+    {
+
+    }
+}
